0107149756fb511cecd070a609486cb2bede0eaa
CabaVS <valerii.savchenko@icloud.com>
2025-07-27 13:59:24 +0200

    Refactor Azure DevOps Mate configuration to use parameterized URL and update project reference name

--- aspire/CabaVS.IdeaForge.AppHost/AppHost.cs
+++ aspire/CabaVS.IdeaForge.AppHost/AppHost.cs
@@ -2,6 +2,10 @@ using Aspire.Hosting.Azure;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+// Parameters
+IResourceBuilder<ParameterResource> configUrlForAzureDevOpsMate =
+    builder.AddParameter("azuredevopsmate-config-url", true);
+
 // Storage Account (emulated with Azurite)
 IResourceBuilder<AzureStorageResource> azurite = builder.AddAzureStorage("stcabavsideaforge")
     .RunAsEmulator(config => config
@@ -13,10 +17,8 @@ IResourceBuilder<AzureStorageResource> azurite = builder.AddAzureStorage("stcaba
 IResourceBuilder<AzureBlobStorageResource> blobsResource = azurite.AddBlobs("blobs");
 
 // Azure DevOps Mate
-builder.AddProject<Projects.CabaVS_AzureDevOpsMate>("aca-azuredevopsmate")
-    .WithEnvironment(
-        "CVS_CONFIGURATION_FROM_AZURE_URL", 
-        "http://127.0.0.1:27000/devstoreaccount1/app-configs/proj-azuredevopsmate.local.json")
+builder.AddProject<Projects.CabaVS_AzureDevOpsMate>("aca-azuredevopsmateapi")
+    .WithEnvironment("CVS_CONFIGURATION_FROM_AZURE_URL", configUrlForAzureDevOpsMate)
     .WithReference(blobsResource).WaitFor(blobsResource);
 
 await builder.Build().RunAsync();
