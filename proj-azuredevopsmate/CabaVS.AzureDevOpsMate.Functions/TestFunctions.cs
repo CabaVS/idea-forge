c4d4532a398a4e0a96ceb1306aab0fff57f0c4b6
CabaVS <valerii.savchenko@icloud.com>
2025-07-29 20:27:16 +0200

    Add `SanityPing` Azure Function for scheduled "ping" responses

---
+++ proj-azuredevopsmate/CabaVS.AzureDevOpsMate.Functions/TestFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Azure.Functions.Worker;
+
+namespace CabaVS.AzureDevOpsMate.Functions;
+
+[SuppressMessage("Style", "IDE0060:Remove unused parameter")]
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
+public class SanityPing
+{
+    [Function("ping")]
+    public string Ping(
+        [TimerTrigger("*/5 * * * * *")] TimerInfo timer,
+        FunctionContext context) => "pong";
+}
