d8ff1a9a49fa1989ed3311c7a254746f6be28429
CabaVS <valerii.savchenko@icloud.com>
2025-07-30 16:33:32 +0200

    Replace SP object ID with App Client ID in role assignments within post-deployment script.

--- infrastructure/scripts/post-deployment-entraid.ps1
+++ infrastructure/scripts/post-deployment-entraid.ps1
@@ -36,38 +36,38 @@ if (-not [string]::IsNullOrWhiteSpace($Environment)) {
     $AppName = "$AppName-$Environment"
 }
 
-Write-Host "-> Acquiring SP object ID: $AppName"
-$SpObjectId = az ad sp list --display-name $AppName --query "[0].id" --output tsv
+Write-Host "-> Acquiring App Client ID: $AppName"
+$AppClientId = az ad app list --display-name $AppName --query "[0].appId" --output tsv
 
 if ($LASTEXITCODE -ne 0) {
-    throw "Failed to acquire SP object ID for '$AppName'"
+    throw "Failed to acquire App Client ID for '$AppName'"
 }
 
-Write-Host "-> Assigning role 'AcrPush' to SP: $SpObjectId"
+Write-Host "-> Assigning role 'AcrPush': $AppClientId"
 $AcrName = "acrcabavsideaforge"
 if (-not [string]::IsNullOrWhiteSpace($Environment)) {
     $AcrName = "$AcrName$Environment"
 }
 
 az role assignment create `
-    --assignee-object-id $SpObjectId `
+    --assignee $AppClientId `
     --role "AcrPush" `
     --scope "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ContainerRegistry/registries/$AcrName" | Out-Null
 
 if ($LASTEXITCODE -ne 0) {
-    throw "Failed to assign the role 'AcrPush' to the SP"
+    throw "Failed to assign the role 'AcrPush'"
 }
 
-Write-Host "-> Assigning role 'Storage Blob Data Contributor' to SP: $SpObjectId"
+Write-Host "-> Assigning role 'Storage Blob Data Contributor' to: $AppClientId"
 
 $StorageAccountName = az storage account list --resource-group $ResourceGroupName --query "[0].name" --output tsv
-$FunctionsReleasesContainerName = 'function-releases'
+$FunctionsReleasesContainerName = "function-releases"
 
 az role assignment create `
-    --assignee-object-id $SpObjectId `
+    --assignee $AppClientId `
     --role "Storage Blob Data Contributor" `
     --scope "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.Storage/storageAccounts/$StorageAccountName/blobServices/default/containers/$FunctionsReleasesContainerName" | Out-Null
 
 if ($LASTEXITCODE -ne 0) {
-    throw "Failed to assign the role 'Storage Blob Data Contributor' to the SP"
+    throw "Failed to assign the role 'Storage Blob Data Contributor'"
 }
