889cef548e2aa60342e5a63a63ef04d8496146e0
CabaVS <valerii.savchenko@icloud.com>
2025-08-20 08:34:24 +0200

    Update `main.tf`: disable external ingress connections for Container App

--- infrastructure/terraform/proj-azuredevopsmate/main.tf
+++ infrastructure/terraform/proj-azuredevopsmate/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_container_app" "app_azuredevopsmate" {
 
   ingress {
     allow_insecure_connections = false
-    external_enabled           = true
+    external_enabled           = false
     target_port                = 8080
     transport                  = "auto"
 
